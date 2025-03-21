042835fb4d92a2bc3a23e138549116533bda3f49
qheizen <accmaincenter@gmail.com>
2025-03-22 01:40:57 +0300

    [fix] allmatrix

--- KoshiDerivation/Derivation.py
+++ KoshiDerivation/Derivation.py
@@ -5,20 +5,20 @@ from typing import List
 np.set_printoptions(precision=30, linewidth=200)
 
 HIGH_PRECISION_DTYPE = np.float64
-
+MATRIX_SIZE = 3
 
 def generate_permutation_matrices() -> List[np.ndarray]:
     matrices = []
-    for perm in permutations(range(3)):
-        matrix = np.zeros((3, 3), dtype=HIGH_PRECISION_DTYPE)
+    for perm in permutations(range(MATRIX_SIZE)):
+        matrix = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype=HIGH_PRECISION_DTYPE)
         for row, col in enumerate(perm):
             matrix[row, col] = HIGH_PRECISION_DTYPE(1.0)
         matrices.append(matrix)
     return matrices
 
 
-def generate_diagonal_matrices() -> List[np.ndarray]:
-    diag_combinations = product([1.0, -1.0], repeat=3)
+def generate_refraction_matrices() -> List[np.ndarray]:
+    diag_combinations = product([1.0, -1.0], repeat=MATRIX_SIZE)
     matrices = []
     for combination in diag_combinations:
         matrix = np.diag([HIGH_PRECISION_DTYPE(v) for v in combination])
@@ -26,17 +26,21 @@ def generate_diagonal_matrices() -> List[np.ndarray]:
     return matrices
 
 
-def random_matrix_calculate(size: int = 3) -> np.ndarray:
+def random_matrix_calculate(size: int = MATRIX_SIZE) -> np.ndarray:
     matrix = np.random.rand(size, size).astype(HIGH_PRECISION_DTYPE)
     matrix *= np.random.randint(0,10000)
     return matrix
 
 
 def calculate_determinant(matrix: np.ndarray) -> list:
-    submatrix = np.array([[matrix[1, 1], matrix[1, 2]], [matrix[2, 1], matrix[2, 2]]], dtype=HIGH_PRECISION_DTYPE)
-    submatrix1 = np.array([[matrix[1, 0], matrix[1, 2]], [matrix[2, 0], matrix[2, 2]]], dtype=HIGH_PRECISION_DTYPE)
-    submatrix2 = np.array([[matrix[1, 0], matrix[1, 1]], [matrix[2, 0], matrix[2, 1]]], dtype=HIGH_PRECISION_DTYPE)
-    return [np.linalg.det(submatrix), np.linalg.det(submatrix1), np.linalg.det(submatrix2)]
+    rows, cols = matrix.shape
+    max_k = min(rows, cols)
+    determinants = []
+    for k in range(1, max_k + 1):
+        minor = matrix[:k, :k]
+        det = np.linalg.det(minor)
+        determinants.append(det)
+    return determinants
 
 
 def is_equal(a: HIGH_PRECISION_DTYPE, b: HIGH_PRECISION_DTYPE) -> bool:
@@ -46,11 +50,8 @@ def is_equal(a: HIGH_PRECISION_DTYPE, b: HIGH_PRECISION_DTYPE) -> bool:
 
 
 def get_orthogonal_random_matrix() -> np.ndarray:
-    random_matrix = random_matrix_calculate()
+    random_matrix = random_matrix_calculate(MATRIX_SIZE)
     Q, R = np.linalg.qr(random_matrix.astype(HIGH_PRECISION_DTYPE))
-    det = np.linalg.det(Q)
-    if det < 0:
-        Q[:, 0] *= -1
     return Q
 
 
@@ -63,7 +64,7 @@ def matrix_product_combinations(matrix_list1: List[np.ndarray], matrix_list2: Li
     return products
 
 
-def get_all_variation_transitive() -> List[np.ndarray]:
-    diagonal_matrices = generate_diagonal_matrices()
+def get_all_variations_of_pe() -> List[np.ndarray]:
+    diagonal_matrices = generate_refraction_matrices()
     permutation_matrices = generate_permutation_matrices()
     return matrix_product_combinations(diagonal_matrices, permutation_matrices)
--- main.py
+++ main.py
@@ -1,40 +1,13 @@
+from math import factorial
+
+import numpy
 import KoshiDerivation.Derivation as ksh
-
-ROUND = 4
-
-def unlimit_checker(transitive_matrices: list):
-    while True:
-        matrix1 = ksh.get_orthogonal_random_matrix()
-        matrix2 = ksh.get_orthogonal_random_matrix()
-
-        variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
-
-        for i, v in enumerate(variants):
-            det = map(abs, ksh.calculate_determinant(v))
-            elem = map(abs, [v[0][0], v[0][1], v[0][2]])
-
-            if check_is_matrix_abnormal(det):
-                print_abnormal_matrix(det, elem, v, matrix1, transitive_matrices[i], matrix2, i)
-
-
-def single_matrix_gen(transitive_matrices: list):
-    matrix1 = ksh.get_orthogonal_random_matrix()
-    matrix2 = ksh.get_orthogonal_random_matrix()
-
-    variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
-
-    for i, v in enumerate(variants):
-        det = list(map(abs, ksh.calculate_determinant(v)))
-        elem = list(map(abs, [v[0][0], v[0][1], v[0][2]]))
-
-        if check_is_matrix_abnormal(det):
-            print_abnormal_matrix(det, elem, v, matrix1, transitive_matrices[i], matrix2, i)
-        else:
-            print_all_about_matrix(det, elem, v, i)
-
+import os
+ROUND_COEF = 4
+STAT_COUNT = 10000
 
 def print_all_about_matrix(dets, elems, matrix, num):
-    print("\033[37m#" * 70)
+    print("#" * 70)
     print(f"Номер матрицы - {num + 1}".center(60))
     print("Матрица после перемножения:")
     print(matrix)
@@ -42,43 +15,277 @@ def print_all_about_matrix(dets, elems, matrix, num):
     print("Детерминанты миноров (модуль):", list(map(float, dets)))
 
 
-def print_abnormal_matrix(dets, elems, matrix, matrix1, t, matrix2, num):
-    print("\033[36m#" * 70)
+def print_abnormal_matrix(dets, matrix, matrix1, t, matrix2, num):
+    print("#" * 70)
     print(f"Номер матрицы - {num+1}".center(60))
-    print("Q1:")
-    print(matrix1.round(ROUND))
-    print("P:")
+    print("Q1: det(Q1) = ", numpy.linalg.det(matrix1))
+    print(matrix1.round(ROUND_COEF))
+    print("P: det(P) = ", numpy.linalg.det(t))
     print(t)
-    print("Q2:")
-    print(matrix2.round(ROUND))
+    print("Q2: det(Q2) = ", numpy.linalg.det(matrix2))
+    print(matrix2.round(ROUND_COEF))
     print("Матрица после перемножения:")
-    print(matrix.round(ROUND))
-    print("Элементы матрицы первой строки (модуль):", list(map(float, elems)))
+    print(matrix.round(ROUND_COEF))
     print("Детерминанты миноров (модуль):", list(map(float,dets)))
-    print("\033[37m")
 
 
 def check_is_matrix_abnormal(det):
-    return min(det) >= 1/24
+    return min(det) >= 1/(factorial(ksh.MATRIX_SIZE) * (2**(ksh.MATRIX_SIZE-1)))
 
+# для 4 порядка - 1/8*24
+# для 5 1/5факториал*2^4
+
+def set_matrix_size():
+    print(f"Укажите значение типа int")
+    ksh.MATRIX_SIZE = int(input())
+
+def set_stat_size():
+    print(f"Укажите значение типа int")
+    STAT_COUNT = int(input())
+
+def calc_single_pe_abs(): #1
+    transitive_matrices = ksh.get_all_variations_of_pe()
+    matrix1 = ksh.get_orthogonal_random_matrix()
+    matrix2 = ksh.get_orthogonal_random_matrix()
+
+    variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+
+    counter = 0
+    for i, v in enumerate(variants):
+        det = ksh.calculate_determinant(v)
+
+        if check_is_matrix_abnormal(list(map(abs,det))):
+            print_abnormal_matrix(det, v, matrix1, transitive_matrices[i], matrix2, i)
+            counter += 1
+    print("Количество матриц - ", counter)
+
+def calc_single_p_abs(): #2
+    transitive_matrices = ksh.generate_permutation_matrices()
+    matrix1 = ksh.get_orthogonal_random_matrix()
+    matrix2 = ksh.get_orthogonal_random_matrix()
+
+    variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+
+    counter = 0
+    for i, v in enumerate(variants):
+        det = ksh.calculate_determinant(v)
+
+        if check_is_matrix_abnormal(list(map(abs,det))):
+            print_abnormal_matrix(det, v, matrix1, transitive_matrices[i], matrix2, i)
+            counter += 1
+    print("Количество матриц - ", counter)
+
+def calc_single_pe(): #3
+    transitive_matrices = ksh.get_all_variations_of_pe()
+    matrix1 = ksh.get_orthogonal_random_matrix()
+    matrix2 = ksh.get_orthogonal_random_matrix()
+
+    variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+
+    counter = 0
+    for i, v in enumerate(variants):
+        det = ksh.calculate_determinant(v)
+
+        if check_is_matrix_abnormal(det):
+            print_abnormal_matrix(det, v, matrix1, transitive_matrices[i], matrix2, i)
+            counter += 1
+    print("Количество матриц - ", counter)
+
+def calc_single_p(): #4
+    transitive_matrices = ksh.generate_permutation_matrices()
+    matrix1 = ksh.get_orthogonal_random_matrix()
+    matrix2 = ksh.get_orthogonal_random_matrix()
+
+    variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+
+    counter = 0
+    for i, v in enumerate(variants):
+        det = ksh.calculate_determinant(v)
+
+        if check_is_matrix_abnormal(det):
+            print_abnormal_matrix(det, v, matrix1, transitive_matrices[i], matrix2, i)
+            counter += 1
+    print("Количество матриц - ", counter)
+
+def calc_many_pe_abs():
+    transitive_matrices = ksh.get_all_variations_of_pe()
+    index_stat = [0] * (len(transitive_matrices) + 1)
+    counter_anti = 0
+    for k in range(STAT_COUNT):
+        matrix1 = ksh.get_orthogonal_random_matrix()
+        matrix2 = ksh.get_orthogonal_random_matrix()
+
+        variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+        counter = 0
+
+
+        for i, v in enumerate(variants):
+            det = ksh.calculate_determinant(v)
+
+            if check_is_matrix_abnormal(list(map(abs,det))):
+                index_stat[i] += 1
+                counter += 1
+        print("Количество матриц - ",  counter)
+
+        if counter == 0:
+            print("Нет удовлетворяющих матриц")
+            print(matrix1)
+            print(numpy.linalg.det(matrix1))
+            print(matrix2)
+            print(numpy.linalg.det(matrix2))
+            counter_anti += 1
+            index_stat[len(transitive_matrices)] += 1
+
+        del matrix1
+        del matrix2
+
+    print("Количество удовлетворяющих - ", counter_anti)
+    print("Статистика по индексам - ", index_stat)
+
+
+def calc_many_p_abs():
+    transitive_matrices = ksh.generate_permutation_matrices()
+    index_stat = [0] * (len(transitive_matrices) + 1)
+    counter_anti = 0
+    for k in range(STAT_COUNT):
+        matrix1 = ksh.get_orthogonal_random_matrix()
+        matrix2 = ksh.get_orthogonal_random_matrix()
+
+        variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+        counter = 0
+
+        for i, v in enumerate(variants):
+            det = ksh.calculate_determinant(v)
+
+            if check_is_matrix_abnormal(list(map(abs,det))):
+                index_stat[i] += 1
+                counter += 1
+        print("Количество матриц - ", counter)
+
+        if counter == 0:
+            print("Нет удовлетворяющих матриц")
+            print(matrix1)
+            print(numpy.linalg.det(matrix1))
+            print(matrix2)
+            print(numpy.linalg.det(matrix2))
+            counter_anti += 1
+            index_stat[len(transitive_matrices)] += 1
+
+        del matrix1
+        del matrix2
+    print("Количество удовлетворяющих - ", counter_anti)
+    print("Статистика по индексам - ", index_stat)
+
+
+def calc_many_pe():
+    transitive_matrices = ksh.get_all_variations_of_pe()
+    index_stat = [0] * (len(transitive_matrices) + 1)
+    counter_anti = 0
+    for k in range(STAT_COUNT):
+        matrix1 = ksh.get_orthogonal_random_matrix()
+        matrix2 = ksh.get_orthogonal_random_matrix()
+
+        variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+        counter = 0
+
+        for i, v in enumerate(variants):
+            det = ksh.calculate_determinant(v)
+
+            if check_is_matrix_abnormal(det):
+                index_stat[i] += 1
+                counter += 1
+        print("Количество матриц - ", counter)
+
+        if counter == 0:
+            print("Нет удовлетворяющих матриц")
+            print(matrix1)
+            print(numpy.linalg.det(matrix1))
+            print(matrix2)
+            print(numpy.linalg.det(matrix2))
+            counter_anti += 1
+            index_stat[len(transitive_matrices)] += 1
+
+        del matrix1
+        del matrix2
+    print("Количество удовлетворяющих - ", counter_anti)
+    print("Статистика по индексам - ", index_stat)
+
+def calc_many_p():
+    transitive_matrices = ksh.generate_permutation_matrices()
+    index_stat = [0] * (len(transitive_matrices) + 1)
+    counter_anti = 0
+    for k in range(STAT_COUNT):
+        matrix1 = ksh.get_orthogonal_random_matrix()
+        matrix2 = ksh.get_orthogonal_random_matrix()
+
+        variants = [matrix1 @ t @ matrix2 for t in transitive_matrices]
+        counter = 0
+
+        for i, v in enumerate(variants):
+            det = ksh.calculate_determinant(v)
+
+            if check_is_matrix_abnormal(det):
+                index_stat[i] += 1
+                counter += 1
+        print("Количество матриц - ", counter)
+
+        if counter == 0:
+            print("Нет удовлетворяющих матриц")
+            print(matrix1)
+            print(numpy.linalg.det(matrix1))
+            print(matrix2)
+            print(numpy.linalg.det(matrix2))
+            counter_anti += 1
+            index_stat[len(transitive_matrices)] += 1
+
+        del matrix1
+        del matrix2
+    print("Количество удовлетворяющих - ", counter_anti)
+    print("Статистика по индексам - ", index_stat)
 
 def main():
-    transitive_matrices = ksh.get_all_variation_transitive()
-    print(len(transitive_matrices))
-    print("Введите 1 - работа в бесконечном режиме с отслеживанием условия.")
-    print("Введите 2 - вывести все транзитивные матрицы.")
-    print("Иначе нажмите Enter")
+    while True:
+        os.system('cls')
+        print(f"Для указания размерности матрицы введите -> size")
+        print(f"Для указания количества просчитываемых матриц в режиме статистики -> stat")
+        print(f"Прогонки с матрицей {ksh.MATRIX_SIZE} перестановок и отражения P @ E и взятия детерминанта по модулю -> 1")
+        print(f"Прогонки с матрицей {ksh.MATRIX_SIZE} перестановок P и взятия детерминанта по модулю -> 2")
+        print(f"Прогонки с матрицей {ksh.MATRIX_SIZE} перестановок и отражения P @ E -> 3")
+        print(f"Прогонки с матрицей {ksh.MATRIX_SIZE} перестановок P -> 4")
+        print(f"Просчет {STAT_COUNT} матриц с P @ E и взятия детерминанта по модулю -> 5")
+        print(f"Просчет {STAT_COUNT} матриц с P и взятия детерминанта по модулю -> 6")
+        print(f"Просчет {STAT_COUNT} матриц с P @ E -> 7")
+        print(f"Просчет {STAT_COUNT} матриц с P -> 8")
 
-    inputStr: str = input()
+        input_string: str = input()
 
-    if inputStr == "1":
-        unlimit_checker(transitive_matrices)
-    elif inputStr == "2":
-        for matrix in transitive_matrices:
-            print(matrix)
-    else:
-        single_matrix_gen(transitive_matrices)
+        if input_string == "size":
+            set_matrix_size()
+        elif input_string == "stat":
+            set_stat_size()
+        elif input_string == "1":
+            calc_single_pe_abs()
+        elif input_string == "2":
+            calc_single_p_abs()
+        elif input_string == "3":
+            calc_single_pe()
+        elif input_string == "4":
+            calc_single_p()
+        elif input_string == "5":
+            calc_many_pe_abs()
+        elif input_string == "6":
+            calc_many_p_abs()
+        elif input_string == "7":
+            calc_many_pe()
+        elif input_string == "8":
+            calc_many_p()
+
+        input("Нажмите enter")
+        input()
+
 
 
 if __name__ == "__main__":
     main()
+
+
